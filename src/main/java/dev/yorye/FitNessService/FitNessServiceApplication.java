0cea55290d0aac3b4befebc222f34fa3fad162a6
Yorye <yorye.dev@gamil.com>
2024-02-27 22:15:38 +0100

    [1]: Inicio proyecto spring para api FitNess

---
+++ src/main/java/dev/yorye/FitNessService/FitNessServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.yorye.FitNessService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitNessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitNessServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yorye/FitNessService/FitNessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yorye.FitNessService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitNessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
